93a03d699cb6aefb7706fb593b07e2500e5f0653
Celina Alsina <celina.alsina@gmail.com>
2021-10-04 10:25:08 +0200

    funcionalidad menú hamburguesa

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
-//VARIABLES HEADER
+//VARIABLES HEADER/NAV
 
 const buttonBalance = document.getElementById("open-balance-section")
 const buttonCategorias = document.getElementById("open-categorias-section")
 const buttonReportes = document.getElementById("open-reportes-section")
+const burgerNavButton = document.getElementById("button-navbar-burger")
+const windowNavBarOpen =document.getElementById("burger-navbar-open")
 
 
 //Variables main
@@ -11,7 +13,7 @@ const sectionBalance = document.getElementById("section-balance")
 const sectionCategorias = document.getElementById("section-categorias")
 const sectionReportes = document.getElementById("section-reportes")
 
-//Funcionalidad Header
+//Funcionalidad Header/Nav
 
 buttonBalance.onclick = () => {
     sectionCategorias.classList.add('is-hidden')
@@ -29,4 +31,10 @@ buttonReportes.onclick = () => {
     sectionBalance.classList.add('is-hidden')
     sectionCategorias.classList.add('is-hidden')
     sectionReportes.classList.remove('is-hidden')
+}
+
+burgerNavButton.onclick = () => {
+    burgerNavButton.classList.toggle('is-active')
+    windowNavBarOpen.classList.toggle('is-active')
+
 }
